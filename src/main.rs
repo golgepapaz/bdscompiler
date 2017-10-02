6ee0f85d5ce8da58d129c68005bd5ac9f380117d
hurcan.solter <hurcan.solter@simsoft.com.tr>
2017-10-02 18:00:29 +0300

    specs goes here

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+
+#![allow(dead_code)]
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::*;
 use std::fs::File;
 use std::error::Error;
 use std::io::prelude::*;
+
+mod spec;
+use spec::*;
 fn main() {
+
+    let _t : ClassSpec ;
 
    let mut file = match File::open("IBSMessage.yaml") {
        Err(why) => panic!("could not open file : {}",why.description()),
@@ -15,37 +22,37 @@ fn main() {
    let docs = YamlLoader::load_from_str(&s).unwrap();
    let doc = &docs[0]; println!("{:?}", doc);
 
-    // Index access for map & array
-    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
-    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
-
-    // Chained key/array access is checked and won't panic,
-    // return BadValue if they are not exist.
-    assert!(doc["INVALID_KEY"][100].is_badvalue());
-
-    // Dump the YAML object
-    let mut out_str = String::new();
-    {
-        let mut emitter = YamlEmitter::new(&mut out_str);
-               emitter.dump(doc).unwrap(); // dump the YAML object to a String
-    }
-    println!("{}", out_str); println!("{:?}", doc);
-
-    // Index access for map & array
-    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
-    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
-
-    // Chained key/array access is checked and won't panic,
-    // return BadValue if they are not exist.
-    assert!(doc["INVALID_KEY"][100].is_badvalue());
-
-    // Dump the YAML object
-    let mut out_str = String::new();
-    {
-        let mut emitter = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc).unwrap(); // dump the YAML object to a String
-    }
-    println!("{}", out_str);
+//    // Index access for map & array
+//    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+//    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
+//
+//    // Chained key/array access is checked and won't panic,
+//    // return BadValue if they are not exist.
+//    assert!(doc["INVALID_KEY"][100].is_badvalue());
+//
+//    // Dump the YAML object
+//    let mut out_str = String::new();
+//    {
+//        let mut emitter = YamlEmitter::new(&mut out_str);
+//               emitter.dump(doc).unwrap(); // dump the YAML object to a String
+//    }
+//    println!("{}", out_str); println!("{:?}", doc);
+//
+//    // Index access for map & array
+//    assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+//    assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
+//
+//    // Chained key/array access is checked and won't panic,
+//    // return BadValue if they are not exist.
+//    assert!(doc["INVALID_KEY"][100].is_badvalue());
+//
+//    // Dump the YAML object
+//    let mut out_str = String::new();
+//    {
+//        let mut emitter = YamlEmitter::new(&mut out_str);
+//        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+//    }
+//    println!("{}", out_str);
 
 
  
